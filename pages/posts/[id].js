5f79307dfc87358ff27fe27423631301d6ff1a39
syukuri <souki.shinohara@outlook.jp>
2023-01-20 12:58:04 +0900

    all done

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import utilStyle from "../../styles/utils.module.css";
+
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+
+  console.log(postData);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyle.headingX1}>{postData.title}</h1>
+        <div className={utilStyle.lightText}>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }}></div>
+      </article>
+    </Layout>
+  );
+}
